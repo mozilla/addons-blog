87fbee727d0988068ce9d6e2ba049f7d4c132a6a
William Durand <will+git@drnd.me>
2021-03-26 11:32:42 +0100

    Use DOMPurify (#22)

--- libs/templates.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const Nunjucks = require('nunjucks');
-
-module.exports = ((eleventyConfig) => {
-  let nunjucksEnvironment = new Nunjucks.Environment(
-    [new Nunjucks.FileSystemLoader('includes')],
-    { autoescape: true }
-  );
-})();
