bf6a41dbbd819e202819c3847888c344711fd565
William Durand <will+git@drnd.me>
2021-03-29 11:17:17 +0200

    Fix post data and rewrite internal URLs (#35)

---
+++ tests/fixtures/apiPost.js
@@ -0,0 +1,38 @@
+module.exports = {
+  id: 1,
+  date: '2021-03-15T14:06:29',
+  date_gmt: '2021-03-15T14:06:29',
+  guid: { rendered: 'http://mozamo.wpengine.com/?p=1' },
+  modified: '2021-03-23T22:04:41',
+  modified_gmt: '2021-03-23T22:04:41',
+  slug: 'hello-world',
+  status: 'publish',
+  type: 'post',
+  link: 'https://mozamo.wpengine.com/2021/03/15/hello-world/',
+  title: {
+    rendered: 'Hello, World',
+  },
+  content: {
+    rendered: '\n' + '<p>some content</p>\n' + '\n' + '<p>more content</p>\n',
+    protected: false,
+  },
+  excerpt: {
+    rendered: '<p>some content</p>\n',
+    protected: false,
+  },
+  author: 1,
+  featured_media: 0,
+  comment_status: 'open',
+  ping_status: 'open',
+  sticky: false,
+  template: '',
+  format: 'standard',
+  meta: [],
+  categories: [1],
+  tags: [],
+  yoast_head:
+    '<!-- This site is optimized with the Yoast SEO plugin v16.0.2 - https://yoast.com/wordpress/plugins/seo/ -->\n' +
+    '<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />\n' +
+    '<script type="application/ld+json" class="yoast-schema-graph">{"@context":"https://schema.org","@graph":[]}</script>\n' +
+    '<!-- / Yoast SEO plugin. -->',
+};
