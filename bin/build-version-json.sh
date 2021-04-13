75f165b9bb38fcd762795af572d3dd33c07eb601
bqbn <bqbn@openken.com>
2021-04-13 15:07:49 -0700

    Update bin/build-version-json.sh
    Co-authored-by: William Durand <will+git@drnd.me>

--- bin/build-version-json.sh
+++ bin/build-version-json.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)/..
 HASH=$(git --no-pager log --format=format:"%H" -1)
 TAG=$(git describe --tags)
 
-printf '{"commit":"%s","version":"%s","source":"https://github.com/mozilla/extension-workshop"}\n' \
+printf '{"commit":"%s","version":"%s","source":"https://github.com/mozilla/addons-blog"}\n' \
     "$HASH" \
     "$TAG" \
     > version.json
