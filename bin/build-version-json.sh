8beb3513307a32f6e82b681d289035ec63d772e6
William Durand <will+git@drnd.me>
2021-04-26 13:16:13 +0200

    Add buildtime prop to version.json

--- bin/build-version-json.sh
+++ bin/build-version-json.sh
@@ -6,9 +6,10 @@ cd $(dirname $0)/..
 HASH=$(git --no-pager log --format=format:"%H" -1)
 TAG=$(git describe --tags)
 
-printf '{"commit":"%s","version":"%s","source":"https://github.com/mozilla/addons-blog"}\n' \
+printf '{"commit":"%s","version":"%s","source":"https://github.com/mozilla/addons-blog","buildtime":"%s"}\n' \
     "$HASH" \
     "$TAG" \
+    "$(date --utc +%FT%T%Z)" \
     > version.json
 
 cat version.json
