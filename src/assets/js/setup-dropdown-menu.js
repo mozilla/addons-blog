04d53faa1b6749b06a1aa3fde3c112e4f8899d38
William Durand <will+git@drnd.me>
2021-04-29 14:11:56 +0200

    Open 'More...' menu on hover

--- src/assets/js/setup-dropdown-menu.js
+++ src/assets/js/setup-dropdown-menu.js
@@ -1,9 +1,21 @@
 /* global document */
 (function setupDropdownMenu() {
-  if (document.querySelector('.DropdownMenu')) {
-    document.addEventListener('click', (event) => {
-      const dropdownMenu = document.querySelector('.DropdownMenu');
+  const openDropDownMenu = (menu) => {
+    menu.classList.add('DropdownMenu--active');
+    menu
+      .querySelector('.DropdownMenu-items')
+      .setAttribute('aria-hidden', false);
+  };
 
+  const closeDropDownMenu = (menu) => {
+    menu.classList.remove('DropdownMenu--active');
+    menu.querySelector('.DropdownMenu-items').setAttribute('aria-hidden', true);
+  };
+
+  const dropdownMenu = document.querySelector('.DropdownMenu');
+
+  if (dropdownMenu) {
+    document.addEventListener('click', (event) => {
       const { target } = event;
       const { parentElement } = target;
 
@@ -12,19 +24,25 @@
           parentElement.matches('.DropdownMenu-button')) &&
         !dropdownMenu.classList.contains('DropdownMenu--active')
       ) {
-        dropdownMenu.classList.add('DropdownMenu--active');
-        dropdownMenu
-          .querySelector('.DropdownMenu-items')
-          .setAttribute('aria-hidden', false);
+        openDropDownMenu(dropdownMenu);
       } else if (
         target.getAttribute('aria-label') !== 'submenu' &&
         parentElement.getAttribute('aria-label') !== 'submenu' &&
         dropdownMenu.classList.contains('DropdownMenu--active')
       ) {
-        dropdownMenu.classList.remove('DropdownMenu--active');
-        dropdownMenu
-          .querySelector('.DropdownMenu-items')
-          .setAttribute('aria-hidden', true);
+        closeDropDownMenu(dropdownMenu);
+      }
+    });
+
+    dropdownMenu.addEventListener('mouseover', () => {
+      if (!dropdownMenu.classList.contains('DropdownMenu--active')) {
+        openDropDownMenu(dropdownMenu);
+      }
+    });
+
+    dropdownMenu.addEventListener('mouseout', () => {
+      if (dropdownMenu.classList.contains('DropdownMenu--active')) {
+        closeDropDownMenu(dropdownMenu);
       }
     });
   }
