d221245bafcd5fde347948b59b0a1ac1d4c44fba
William Durand <will+git@drnd.me>
2021-08-05 12:50:21 +0200

    Set the right tag in version.json (#278)

--- bin/build-version-json.sh
+++ bin/build-version-json.sh
@@ -4,7 +4,15 @@ echo "$(dirname $0)"
 cd $(dirname $0)/..
 
 HASH=$(git --no-pager log --format=format:"%H" -1)
-TAG=$(git describe --tags)
+
+if [ "$AMO_BASE_URL" == "https://addons.mozilla.org" ]; then
+    TAG=$(git describe --tags --abbrev=0 --match='20*.*.*')
+elif [ "$AMO_BASE_URL" == "https://addons.allizom.org" ]; then
+    TAG=$(git describe --tags --abbrev=0 --match='20*.*.*-stage')
+else
+    # No tag on -dev
+    TAG=""
+fi
 
 printf '{"commit":"%s","version":"%s","source":"https://github.com/mozilla/addons-blog","buildtime":"%s"}\n' \
     "$HASH" \
