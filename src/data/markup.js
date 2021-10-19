619a4910edf19feaccf997422a072c3946491e1f
Bob Silverberg <bob.silverberg@gmail.com>
2021-10-19 15:25:25 -0400

    Change the name of the blog

--- src/data/markup.js
+++ src/data/markup.js
@@ -3,7 +3,7 @@ const { buildFooter, buildHeader } = require('addons-frontend-blog-utils');
 const { AMO_BASE_URL } = require('../wordpress');
 
 module.exports = {
-  siteTitle: 'Firefox Add-on Reviews',
+  siteTitle: 'Firefox Add-ons Blog',
   siteDescription:
     'Download Firefox extensions and themes. They’re like apps for your browser. They can block annoying ads, protect passwords, change browser appearance, and more.',
   baseURL: AMO_BASE_URL,
