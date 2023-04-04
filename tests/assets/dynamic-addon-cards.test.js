60e0d3a94d1f4bfa03d820ca0010e5470e0b8f5c
Mathieu Pillard <mpillard@mozilla.com>
2023-04-04 19:42:36 +0200

    Actually restore is fine, just re-set mocks before each test

--- tests/assets/dynamic-addon-cards.test.js
+++ tests/assets/dynamic-addon-cards.test.js
@@ -66,7 +66,7 @@ describe(__filename, () => {
     });
 
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.restoreAllMocks();
   });
 
   describe('convertToUnavailableCard', () => {
@@ -507,15 +507,16 @@ describe(__filename, () => {
     });
 
     describe('mozAddonManager', () => {
-      const fakeInstallObj = { install: jest.fn() };
-      const fakeMozAddonManager = {
-        createInstall: jest.fn().mockReturnValue(fakeInstallObj),
-      };
-
+      let fakeInstallObj;
+      let fakeMozAddonManager
       let originalNavigator;
 
       beforeEach(() => {
         originalNavigator = { ...navigator };
+        fakeInstallObj  = { install: jest.fn() };
+        fakeMozAddonManager = {
+          createInstall: jest.fn().mockReturnValue(fakeInstallObj),
+        };
         navigator.mozAddonManager = fakeMozAddonManager;
       });
 
